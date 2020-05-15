07d9e3d5d16ba97fd1ffa04c4dd8d554d10e8591
Gustavo <gustavosm994@gmail.com>
2020-05-15 13:34:00 -0300

    finished frontend

---
+++ frontend/src/pages/NewIncident/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { FiArrowLeft } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+
+import './styles.css'
+
+import LogoImg from '../../assets/logo.svg'
+
+export default function NewIncident(){
+  return (
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={LogoImg} alt="Be The Hero" />
+
+          <h1>Cadastar novo caso</h1>
+          <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+
+          <Link className="back-link" to="/profile">
+            <FiArrowLeft size={16} color="#e02041" />
+            Voltar para home
+          </Link>
+        </section>
+
+        <form>
+          <input placeholder="Título do caso" />
+          <textarea placeholder="Descrição" />
+          <input placeholder="Valor em reais" />
+                    
+          <button className='button' type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  )
+}
